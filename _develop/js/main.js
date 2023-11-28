27ec5554722b13a66ce730f29ed633caf0c859b8
Redvus <a.suvorof@gmail.com>
2023-11-28 15:19:27 +0400

    Стилизовал под мобильную версию.

--- _develop/js/main.js
+++ _develop/js/main.js
@@ -3,6 +3,7 @@
 const sliderFront = document.getElementById('sliderFront'),
     objectsTab = document.getElementById('objectsTab'),
     sectionMaterials = document.getElementById('sectionMaterials'),
+    sectionMaterialsAll = document.getElementById('sectionMaterialsAll'),
     wrapperSingle = document.querySelector('.wrapper-single')
 ;
 
@@ -13,7 +14,29 @@ function countersFront() {
     countersAll.counterLoad('counterDistrict', 9);
 }
 
+const wrapper = document.querySelector('.wrapper'),
+    headerMob = document.querySelector('.header'),
+    // headerDesktop = document.querySelector('.header'),
+    headerTopPart = document.querySelector('.header__top_part'),
+    headerTopAdress = document.querySelector('.header__top_adress'),
+    headerTopPartRight = document.querySelector('.header__top_part--right'),
+    headerTopAdressMobile = document.querySelector('.header__top_adress--mobile-right')
+;
+
+function wrapperMob() {
+    wrapper.classList = 'wrapper wrapper--mobile';
+    headerMob.classList = 'header header--mobile';
+    headerTopPart.removeChild(headerTopAdress);
+}
+
+function wrapperDesk() {
+    headerTopPart.removeChild(headerTopAdressMobile);
+    // headerTopPart.removeChild(headerTopAdressMobile);
+}
+
 function initMain() {
+
+
     if (typeof objectsTab) {
         objectsTabs();
     }
@@ -22,6 +45,14 @@ function initMain() {
     }
 
     if (sectionMaterials) {
+        const modalLoadNew = new Modal();
+        modalLoadNew.modalLoad('materialLink_1', 'kraspanmetalcolor/metalcolor-info/');
+        modalLoadNew.modalLoad('materialLink_2', 'kraspancomposite-st/composite-st-info/');
+        modalLoadNew.modalLoad('materialLink_3', 'kraspanfibrocementcolor/fiber-cement-info/');
+        modalLoadNew.modalLoad('materialLink_4', 'kraspanfibrocementcolor-2/fiber-cement-color-info/');
+    }
+
+    if (sectionMaterialsAll) {
         const modalLoadNew = new Modal();
         modalLoadNew.modalLoad('materialLink_1', 'kraspanmetalcolor/metalcolor-info/');
         modalLoadNew.modalLoad('materialLink_2', 'kraspancomposite-st/composite-st-info/');
@@ -36,7 +67,13 @@ function initMain() {
 }
 
 function initMainMobile() {
-
+    if (typeof objectsTab) {
+        objectsTabs();
+    }
+    if (typeof sliderFront) {
+        frontSlider();
+    }
+    wrapperMob();
 }
 
 if (document.body.clientWidth > 820 || screen.width > 820) {
